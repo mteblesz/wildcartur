476ae2cf57219dce3401f48b35ac8a9b4f3271df
mteblesz <mteblesz@gmail.com>
2026-05-25 00:33:25 +0200

    map openapi on prod

--- backend/servartur/src/Servartur.Api/Program.cs
+++ backend/servartur/src/Servartur.Api/Program.cs
@@ -4,16 +4,13 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+app.MapOpenApi();
 
 app.UseHttpsRedirection();
 
 app.MapGet("/hello", () =>
 {
-    return "<h1>Hello, Kinga! Greeting from Raspberry Pi!</h1>";
+    return "Hello, Kinga! Greeting from Raspberry Pi!";
 })
 .WithName("GetHello");
 
